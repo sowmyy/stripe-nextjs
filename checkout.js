3443bbdfaca4dd7fae13f3aab8950dfa040ce4cc
sowmyanarayanan.s <sowmyanarayanan.s@canvas.space@gmail.com>
2022-07-11 19:40:32 +0530

    push latest

--- checkout.js
+++ checkout.js
@@ -12,10 +12,18 @@ export async function checkout({lineItems}) {
 
     const stripe = await getStripe()
 
+
+
     await stripe.redirectToCheckout({
         mode: 'payment',
         lineItems,
         successUrl: `${window.location.origin}? session_id={CHECKOUT_SESSION_ID}/successPurchase`,
         cancelUrl: window.location.origin
     })
+
+    await stripe.retrievePaymentIntent('{PAYMENT_INTENT_CLIENT_SECRET}')
+        .then(function(result) {
+        console.log('result', result)
+        // Handle result.error or result.paymentIntent
+    });
  }
